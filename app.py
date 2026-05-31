9b53dcf08ca03a6b624b7ed007a7a7ac6942503c
Jawain08 <jawainswint@gmail.com>
2026-05-31 09:44:25 -0400

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ auto_period_start = ANCHOR_DATE + datetime.timedelta(days=completed_periods * 14
 auto_period_end = auto_period_start + datetime.timedelta(days=13)
 
 # --- STEP 2: PROFILE FILTER CONFIGURATION ---
-st.subheader("🗓️ Pay Period Review Settings")
+st.subheader("🗓️ Current Pay Period ")
 col_profile1, col_profile2 = st.columns(2)
 
 with col_profile1:
